6d37be9faccb8b64457bd84e0230e78c67f3c6ed
Ariunbold <ariun1118@gmail.com>
2019-08-27 21:50:41 -0400

    added some design

--- src/components/nasapic.js
+++ src/components/nasapic.js
@@ -1,6 +1,9 @@
 import React, { } from 'react'
 
 function NasaPic({nasaImg}){
+    if (!nasaImg) 
+    return <h3>Loading....</h3>
+    else 
     return <div>
         <img src = {nasaImg} alt = 'Nasa Pic' />
     </div>
