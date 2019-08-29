53c6d83bc739a76651cea5a1642e7b1b3fa66811
Ariunbold <ariun1118@gmail.com>
2019-08-29 16:29:01 -0400

    empty array dependency added

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         .catch(err => {
           console.log(err)
         })
- })
+ }, [])
   return (
     <div className="App">
      <Title nasaTitle= {nasaTitle} nasaInfo = {nasaInfo} nasaDate = {nasaDate} />
