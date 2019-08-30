af29ab892134296b487dce6cfdff9286370a552f
Ariunbold <ariun1118@gmail.com>
2019-08-29 20:00:54 -0400

    Added Bootstrap styles

--- src/components/nasapic.js
+++ src/components/nasapic.js
@@ -1,12 +1,45 @@
 import React, { } from 'react'
+import {Container,Image, Jumbotron,ButtonToolbar, Button,Spinner   } from 'react-bootstrap'
 
-function NasaPic({nasaImg}){
+function NasaPic({nasaImg, nasaDate, nasaCopyright}){
     if (!nasaImg) 
-    return <h3>Loading....</h3>
-    else 
-    return <div>
-        <img src = {nasaImg} alt = 'Nasa Pic' />
-    </div>
-}
+    return (
+        <ButtonToolbar>
+          <Button variant="primary" disabled>
+            <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+            />
+            <span className="sr-only">Loading...</span>
+            </Button>
+            <Button variant="primary" disabled>
+            <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+            />
+            Loading...
+          </Button>
+        </ButtonToolbar>)
+        else 
+    return (
+        <div> 
+         <Image src = {nasaImg} alt = 'Nasa Pic' fluid/>
+          <Jumbotron fluid>
+            <Container>
+                <h3>{nasaDate}</h3>
+             <h4>
+                Image Credit & Copyright : 
+            </h4>
+            <h3>{nasaCopyright}</h3>
+            </Container>
+          </Jumbotron>
+        </div>)
+    }
 
-export default NasaPic
+export default NasaPic;
--- src/components/title.js
+++ src/components/title.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import {Accordion, Card,Jumbotron,Button } from 'react-bootstrap'
 
-function Title({nasaTitle, nasaInfo, nasaDate }){
-    return <div>
-                <h1>{nasaTitle}</h1>
-                <p>{nasaInfo}</p>
-                <h3>{nasaDate}</h3>
-           </div>
-    
+function Title({nasaTitle, nasaInfo }){
+    return  <div>
+            <Jumbotron>
+                <h1>Today's NASA picture!</h1>
+                <p>
+                Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+                </p> 
+                <h2>{nasaTitle}</h2> 
+                    <p>
+                </p>
+            </Jumbotron>
+                <Accordion defaultActiveKey="0">
+                    <Card>
+                    <Accordion.Toggle as={Card.Header} eventKey="0">
+                        <Button variant="primary">Learn more</Button>
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>{nasaInfo}</Card.Body>
+                    </Accordion.Collapse>
+                    </Card>
+                </Accordion> 
+            </div>    
 }
 
 export default Title;
